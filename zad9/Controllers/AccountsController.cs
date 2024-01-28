dc3b9e136534ecc9d17fff751cee77a3c08c247f
lakami <ossowska.kamila1@gmail.com>
2024-01-28 17:33:43 +0100

    commit login

--- zad9/Controllers/AccountsController.cs
+++ zad9/Controllers/AccountsController.cs
@@ -1,6 +1,10 @@
-﻿using System.Text;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using zad8.Models;
 using zad8.Repo;
 
@@ -13,11 +17,13 @@ public class AccountsController : ControllerBase
     
     private readonly ILogger<DoctorsController> _logger;
     private readonly DatabaseContext _context;
+    private readonly IConfiguration _configuration;
     
-    public AccountsController(ILogger<DoctorsController> logger, DatabaseContext context)
+    public AccountsController(ILogger<DoctorsController> logger, DatabaseContext context, IConfiguration configuration)
     {
         _logger = logger;
         _context = context;
+        _configuration = configuration;
     }
     
     
@@ -46,33 +52,58 @@ public class AccountsController : ControllerBase
     }
     
     
-    // [AllowAnonymous]
-    // [HttpPost("login")]
-    // public IActionResult Login(LoginRequest loginRequest)
-    // {
-    //     var account = _context.Account
-    //         .Where(a => a.Login == loginRequest.Login)
-    //         .FirstOrDefault();
-    //     
-    //     string passwordHash = account.Password;
-    //     string crunchPassword = "";
-    //     
-    //     //walidacja hasła
-    //     if (passwordHash != crunchPassword)
-    //     {
-    //     return Unauthorized("Niepoprawne hasło");
-    //     }
-    //     
-    //     Claim[] accountClaims = new[]
-    //     {
-    //         new Claim(ClaimTypes.Name, "Roman"),
-    //         new Claim(ClaimTypes.Role, "admin"),
-    //         new Claim(ClaimTypes.Role, "user")
-    //     };
-    //     
-    //     SymerticSecurityKey key = new SymerticSecurityKey(Encoding.UTF8.GetBytes(_));
-    // }
-    //
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public IActionResult Login(LoginRequest loginRequest)
+    {
+        var account = _context.Account
+            .Where(a => a.Login == loginRequest.Login)
+            .FirstOrDefault();
+        
+        string passwordHash = account.Password;
+        
+        string crunchPassword = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: loginRequest.Password,
+            salt: Encoding.UTF8.GetBytes(account.Salt),
+            prf: KeyDerivationPrf.HMACSHA512,
+            iterationCount: 10000,
+            numBytesRequested: 256 / 8
+        ));
+        
+        //walidacja hasła
+        if (passwordHash != crunchPassword)
+        {
+        return Unauthorized("Niepoprawne hasło");
+        }
+        
+        Claim[] accountClaims = new[]
+        {
+            new Claim(ClaimTypes.Name, account.Login),
+        };
+        
+        SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecretKey"]));
+        SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        
+        JwtSecurityToken token = new JwtSecurityToken(
+            issuer: "http://localhost:5001",
+            audience: "http://localhost:5001",
+            claims: accountClaims,
+            expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: credentials
+        );
+        
+        var refreshToken = Guid.NewGuid().ToString();
+        account.refreshToken = refreshToken;
+        account.refreshTokenExp = DateTime.Now.AddDays(1);
+        _context.SaveChanges();
+        
+        return Ok(new
+        {
+            token = new JwtSecurityTokenHandler().WriteToken(token),
+            refreshToken
+        });
+    }
+    
     
 }
 
